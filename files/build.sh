97a32d8fec5d27b0b5ba91fd5846f2ea45de8b75
Higgsbi <contact@higgsbi.pm>
2025-01-13 16:27:18 -0500

    updated style, moved to posix sh

--- files/build.sh
+++ files/build.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
-BOLD='\e[1m'
-ITALIC='\e[3m'
-RESET='\e[0m'
+#!/bin/sh
+
+BOLD=$(printf '\033[1m')
+ITALIC=$(printf '\033[3m')
+RESET=$(printf '\033[0m')
 
 PROJECT=${PWD##*/}
 LIB="ctk"
@@ -11,34 +12,27 @@ SYSTEM_INSTALL=false
 RUN_TYPE=""
 
 HELP_MESSAGE="
-$BOLD\
-$(basename "$PWD") $RESET- built via cproject
-$BOLD
-Flags:
-$RESET
-\
-  [$ITALIC --clean   $RESET| $ITALIC-C$RESET ]:   removes build directories
-  [$ITALIC --cached  $RESET| $ITALIC-c$RESET ]:   builds without downloading the $LIB library
-  [$ITALIC --run     $RESET| $ITALIC-r$RESET ]:   runs the built file
-  [$ITALIC --debug   $RESET| $ITALIC-d$RESET ]:   runs the built file with valgrind
-  [$ITALIC --release $RESET| $ITALIC-R$RESET ]:   builds with O3 compiler flags
-  [$ITALIC --system  $RESET| $ITALIC-s$RESET ]:   installs executable system wide
-  [$ITALIC --delete  $RESET| $ITALIC-D$RESET ]:   uninstalls executable system wide
-\
-\
-$BOLD
-Examples:
-$RESET
-\
+${BOLD}$(basename "$PWD")${RESET} - built via cproject
+
+${BOLD}Flags:${RESET}
+  [ ${ITALIC}--clean${RESET}  | ${ITALIC}-C${RESET} ]:   removes build directories
+  [ ${ITALIC}--cached${RESET} | ${ITALIC}-c${RESET} ]:   builds without downloading the ${LIB} library
+  [ ${ITALIC}--run${RESET}    | ${ITALIC}-r${RESET} ]:   runs the built file
+  [ ${ITALIC}--debug${RESET}  | ${ITALIC}-d${RESET} ]:   runs the built file with valgrind
+  [ ${ITALIC}--release${RESET}| ${ITALIC}-R${RESET} ]:   builds with O3 compiler flags
+  [ ${ITALIC}--system${RESET} | ${ITALIC}-s${RESET} ]:   installs executable system-wide
+  [ ${ITALIC}--delete${RESET} | ${ITALIC}-D${RESET} ]:   uninstalls executable system-wide
+
+${BOLD}Examples:${RESET}
   ./build.sh --clean                   (clean build directories)
   ./build.sh --cached                  (build without library install)
   ./build.sh --release                 (build without library install with O3 option)
   ./build.sh --cached --run            (build and run without library install)
   ./build.sh --cached --run --release  (build and run without library install with O3 option)
   ./build.sh --cached --debug          (build and debug without library install)
-  ./build.sh --system                  (build and install system wide)
-  ./build.sh --delete                  (uninstalls executable system wide)
-  ./build.sh                           (build without running)    
+  ./build.sh --system                  (build and install system-wide)
+  ./build.sh --delete                  (uninstall executable system-wide)
+  ./build.sh                           (build without running)
 
 "
 
@@ -48,15 +42,15 @@ build() {
     mkdir build
     mkdir out
     cd build || exit 1
-    if [[ $RELEASE == "true" ]]; then
+    if [ "$RELEASE" = "true" ]; then
         cmake -DCMAKE_BUILD_TYPE=Release ..
     else
         cmake -DCMAKE_BUILD_TYPE=Debug ..
     fi
-    if [[ $SYSTEM_INSTALL == "true" ]]; then
+    if [ "$SYSTEM_INSTALL" = "true" ]; then
         sudo make install
     else
-        make 
+        make
         cp "$PROJECT" ../out/
     fi
     
@@ -65,18 +59,18 @@ build() {
 
 libs() {
     git clone "https://github.com/higgsbi/${LIB}.git"
-    cd $LIB || exit 1
+    cd "$LIB" || exit 1
     chmod +x build.sh
     ./build.sh --local
     cp -r out/* ../
     cd ..
-    rm -rf $LIB
+    rm -rf "$LIB"
 }
 
-for i in "$@"; do
-    case $i in
+while [ "$#" -gt 0 ]; do
+    case "$1" in
         -h|--help)
-            echo -e "$HELP_MESSAGE"
+            printf "%s" "$HELP_MESSAGE"
             exit 0
             ;;
         -C|--clean)
@@ -84,7 +78,7 @@ for i in "$@"; do
             rm -rf out
             rm -rf include
             rm -rf lib
-            printf "Directories have been cleared!\n"
+            printf 'Directories have been cleared!\n'
             exit 0
             ;;
         -c|--cached)
@@ -112,17 +106,21 @@ for i in "$@"; do
             sudo rm -I /usr/local/bin/"$PROJECT"
             exit 0
             ;;
+        \?)
+            printf "Invalid option: -%s\n" "$OPTARG" >&2
+            exit 1
+            ;;
     esac
 done
 
-if ! $CACHED || [ ! -d lib ]; then
+if ! "$CACHED" || [ ! -d lib ]; then
     libs
 fi
 
 build
 
-if [[ $RUN_TYPE == "run" ]]; then
-    "${PWD}/out/${PROJECT}" "$@" 
-elif [[ $RUN_TYPE == "debug" ]]; then
+if [ "$RUN_TYPE" = "run" ]; then
+    "${PWD}/out/${PROJECT}" "$@"
+elif [ "$RUN_TYPE" = "debug" ]; then
     valgrind -s --leak-check=full --track-origins=yes "${PWD}/out/${PROJECT}" "$@"
 fi
--- generate.sh
+++ generate.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ $1 == "--help" ]]; then
+if [ "$1" = "--help" ]; then
     printf "CProject: generate C projects quickly\n\nUsage: project project_name cproject_path\n"
     exit 1
 fi
 
-if [[ -z "$2" ]]; then
+if [ -z "$2" ]; then
     printf "No project name was provided\n"
     exit 1
 fi
 
-if [[ -z "$1" ]]; then
+if [ -z "$1" ]; then
     printf "Provide the path for this script's directory\n"        
     exit 1
 fi 
 
-if [[ ! -d "${1}/files" ]]; then
+if [ ! -d "${1}/files" ]; then
     printf "Given script directory path is invalid\n"
     exit 1
 fi 
@@ -23,4 +23,4 @@ fi
 mkdir "$2"
 cp -r "${1}/files/." "$2"
 cd "$2" || exit 1
-printf "%s\n", "Project ${2} has been generated"
+printf "%s\n", "Project ${2} has been generated\n"
